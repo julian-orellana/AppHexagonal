1755890e095ccea9712dee6901f90912f984b41d
julixxx23 <jorellanao12@miumg.edu.gt>
2026-05-11 23:25:31 -0600

    Feat; coments

--- src/main/java/hexagonal/developer/pedido/dominio/exception/PedidoNotFoundException.java
+++ src/main/java/hexagonal/developer/pedido/dominio/exception/PedidoNotFoundException.java
@@ -1,5 +1,6 @@
 package hexagonal.developer.pedido.dominio.exception;
 
+//Lógica centrada en el Global
 public class PedidoNotFoundException extends RuntimeException{
     public PedidoNotFoundException(Long id){
         super("No encontrado por id: " + id);
