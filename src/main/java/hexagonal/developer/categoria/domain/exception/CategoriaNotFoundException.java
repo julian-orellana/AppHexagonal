174adce629fa5546263326ce75a9a9b90446888b
julixxx23 <jorellanao12@miumg.edu.gt>
2026-06-21 20:15:25 -0600

    Comentary

--- src/main/java/hexagonal/developer/categoria/domain/exception/CategoriaNotFoundException.java
+++ src/main/java/hexagonal/developer/categoria/domain/exception/CategoriaNotFoundException.java
@@ -2,7 +2,7 @@ package hexagonal.developer.categoria.domain.exception;
 
 import hexagonal.developer.shared.domain.exception.NotFoundException;
 
-//Captura error
+//Captura error manejable
 public class CategoriaNotFoundException extends NotFoundException {
     public CategoriaNotFoundException(Long id) {
         super("Categoria no encontrada con id: " + id);
